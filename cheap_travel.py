11d948d80985f8405d72149abbf74fcf4bb3a892
aniket0702 <aniketagarwal11133@gmail.com>
2017-07-13 18:56:03 +0530

    Codeforces solution

--- cheap_travel.py
+++ cheap_travel.py
@@ -1,6 +1,8 @@
+import math
 n,m,a,b = map(int ,raw_input().split())
 s1 = 0
 s1+= (n/m)*b
 s1+= (n%m)*a
-s2 = (n/m +1)*b
-print min(s1,s2)
+s2 = int((math.ceil(float(n)/m) )*b)	
+s3 = n*a
+print min(s1,s2,s3)

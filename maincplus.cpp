1d7d5a18fd28ff2ed4fb0714efb10566b2c7f744
Dikshit15 <42082921+Dikshit15@users.noreply.github.com>
2020-10-01 18:11:30 +0530

    Update maincplus.cpp
    Deleting useless headers.

--- maincplus.cpp
+++ maincplus.cpp
@@ -1,30 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-typedef long long ll;
-#define s(n)                        scanf("%d",&n)
-#define sc(n)                       scanf("%c",&n)
-#define sl(n)                       scanf("%lld",&n)
-#define sf(n)                       scanf("%lf",&n)
-#define ss(n)                       scanf("%s",n)
-#define mp 							make_pair
-#define fi 							first
-#define se 							second
-#define maX(a,b)                    ( (a) > (b) ? (a) : (b))
-#define miN(a,b)                    ( (a) < (b) ? (a) : (b))
-#define pi(n)						printf("%d\n",n );
-#define pc(n)						printf("%c\n",n );
-#define wh(n)						int n;s(n);while(n--)
-#define vi(n)  						vector<int> n;
-#define vpi(n) 						vector< pair < int,int> > n;
-
 int main()
 {
-	int n;
-	s(n);
-	wh(x)
-	cout<<n<<endl;
-
+	int n;cin>>n;
+	while(n--)cout<<n<<endl;
+return 0;
 }
 
